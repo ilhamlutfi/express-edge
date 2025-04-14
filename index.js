247e9597a281a60027ba7819e570f1e0de47ec5c
ilhamlutfi <ilhamlutfi153@gmail.com>
2025-04-14 12:11:28 +0700

    add rate limiter global & auth

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import {
 } from './app/helpers/csrf.js';
 import session from 'express-session';
 import flash from 'connect-flash';
+import rateLimit from 'express-rate-limit';
 
 dotenv.config();
 
@@ -19,6 +20,17 @@ const app = express();
 const edge = Edge.create();
 const port = process.env.PORT || 3000;
 const base_url = `http://localhost:${port}`;
+
+// Rate limiter untuk membatasi 100 permintaan per 15 menit dari setiap IP
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 menit
+    max: 100, // Maksimal 100 permintaan per IP dalam 15 menit
+    message: 'Too many requests from this IP, please try again after 15 minutes.',
+    headers: true, // Menyertakan header rate-limit di response
+});
+
+// Terapkan rate limiter di seluruh aplikasi (global)
+app.use(limiter);
 
 app.use(methodOverride("_method")); // Middleware method spoofing
 app.use(express.json()); // for parsing application/json
@@ -43,7 +55,8 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: false // Set to true if using HTTPS
+        secure: false, // Set ke true jika menggunakan HTTPS
+        maxAge: 24 * 60 * 60 * 1000 // Waktu kedaluwarsa sesi 24 jam (24 jam * 60 menit * 60 detik * 1000 milidetik)
     }
 })); // Session middleware
 app.use(flash()); // Flash middleware
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import AuthController from '../app/controllers/AuthController.js';
 import { isAuthenticated } from '../app/middlewares/AuthMiddleware.js';
+import rateLimit from 'express-rate-limit';
 
 const authRoutes = express.Router();
+const authLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 menit
+    max: 10, // Maksimal 10 permintaan per IP dalam 1 menit
+    message: 'Too many requests from this IP, please try again later.',
+    skipFailedRequests: false, // jangan mengabaikan permintaan yang gagal
+    keyGenerator: (req) => req.ip // rate limit berdasarkan IP
+})
 
 authRoutes.get('/login', isAuthenticated, AuthController.login);
-authRoutes.post('/login', isAuthenticated, AuthController.loginAttempt);
+authRoutes.post('/login', authLimiter, isAuthenticated, AuthController.loginAttempt);
 authRoutes.get('/register', AuthController.register);
-authRoutes.post('/register', AuthController.registerAttempt);
+authRoutes.post('/register', authLimiter, AuthController.registerAttempt);
 
 export default authRoutes;
 
